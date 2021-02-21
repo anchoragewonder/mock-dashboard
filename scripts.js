49e44a6ff03fa4262dd6768dc498458867877906
anchoragewonder <anchoragewonder@gmail.com>
2021-02-21 17:19:44 -0500

    toggle class function for card style

--- scripts.js
+++ scripts.js
@@ -7,26 +7,71 @@ $(document).ready(function () {
         $('#sidebar').addClass('active');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
-
-    $('#darkMode').on('click', function () {
-        $('.card').removeClass('text-dark bg-light').addClass('text-light bg-dark');
-    })
-
-    $("#lightMode").on('click', function () {
-        $('.card').removeClass('text-light bg-dark ').addClass('text-dark bg-light');
-    })
 });
 
-function hideButton(buttonId) {
-    document.getElementById(buttonId).style.display = "none";
-    console.log(`${buttonId} was hid`);
+document.addEventListener('DOMContentLoaded', () => {
 
-    if (buttonId == "darkMode") {
-        document.getElementById("lightMode").style.display = "block";
-        console.log('light button should be visable');
+    const themeToggle = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark');
+        themeToggle.classList.toggle('dark');
+        localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+        cardStyleToggle();
+    });
+
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark');
+        themeToggle.classList.add('dark');
+        cardStyleToggle();
     }
-    if (buttonId == "lightMode") {
-        document.getElementById("darkMode").style.display = "block";
-        console.log('dark button should be visable');
+})
+
+function cardStyleToggle() {
+    document.querySelectorAll('.card').forEach(item => {
+        item.classList.toggle('bg-light')
+        item.classList.toggle('bg-dark')
+        item.classList.toggle('text-dark')
+        item.classList.toggle('text-light')
+    })
+
+};
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
     }
-}
+});
